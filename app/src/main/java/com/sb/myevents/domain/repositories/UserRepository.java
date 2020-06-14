49d9ccc81800bb6bbd168ed6c66ef5f3ae4dd6e8
Francisco Sierra <fsierra@dorinter.com>
2020-06-14 02:43:25 -0500

    Added firebase auth and login flow

--- app/src/main/java/com/sb/myevents/domain/repositories/UserRepository.java
+++ app/src/main/java/com/sb/myevents/domain/repositories/UserRepository.java
@@ -1,5 +1,9 @@
 package com.sb.myevents.domain.repositories;
 
+import androidx.lifecycle.Observer;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.sb.myevents.data.entities.User;
 import com.sb.myevents.sys.util.AppExecutors;
 import com.sb.myevents.ui.MainApp;
 
@@ -12,7 +16,45 @@ public class UserRepository {
 
     private AppExecutors executors;
 
+    private FirebaseAuth auth;
+
     public UserRepository() {
         executors = MainApp.utilComponent.getAppExecutors();
+        auth = FirebaseAuth.getInstance();
+    }
+
+    public void isUserSigned(Observer<Object> observer) {
+        executors.networkIO().execute(() -> observer.onChanged(auth.getCurrentUser()));
+    }
+
+    public void logout(Observer<Boolean> observer) {
+        executors.networkIO().execute(() -> {
+            auth.signOut();
+            observer.onChanged(true);
+        });
+    }
+
+    public void createUser(User user, Observer<Object> observer) {
+        auth.createUserWithEmailAndPassword(user.getEmail(), user.getPassword()).addOnCompleteListener(executors.networkIO(), task -> {
+            if (task.isSuccessful()) {
+                // Sign in success, update UI with the signed-in user's information
+                observer.onChanged(auth.getCurrentUser());
+            } else {
+                // If sign in fails, display a message to the user.
+                observer.onChanged(null);
+            }
+        });
+    }
+
+    public void signInUser(String email, String password, Observer<Object> observer) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(executors.networkIO(), task -> {
+            if (task.isSuccessful()) {
+                // Sign in success, update UI with the signed-in user's information
+                observer.onChanged(auth.getCurrentUser());
+            } else {
+                // If sign in fails, display a message to the user.
+                observer.onChanged(null);
+            }
+        });
     }
 }
